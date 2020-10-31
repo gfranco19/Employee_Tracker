64db735a67ea6cffc919dcf57fa6f29eabf660f1
Giovanny Franco <gfranco19@live.com>
2020-10-31 00:01:33 -0700

    udating functions

--- server.js
+++ server.js
@@ -16,12 +16,16 @@ connection.connect(function (err) {
 });
 
 function questions() {
+    connection.query("SELECT employee.id, employee.first_name, employeee.lastname, roles.title, department.name, roles.salary, employee.manager_id FROM employee INNER JOIN roles ON roles.deparment_id = department_id INNER JOIN employee.role_id = employee.role_id", function (err, data) {
+        console.table(data);
+    });
     inquirer.prompt({
         message: "How can i help you today?",
         type: "list",
         choices: [
             "View all employees",
             "View all departments",
+            "View all roles",
             "Add employee",
             "Add department",
             "Add role",
@@ -32,6 +36,7 @@ function questions() {
     }).then(answers => {
         console.log(answers.choice);
         switch (answers.choice) {
+
             case "View all employees":
                 viewEmployees();
                 break;
@@ -57,7 +62,7 @@ function questions() {
                 newRole();
                 break;
 
-            case "update employee role":
+            case "Update employee role":
                 updateRole();
                 break;
 
@@ -89,3 +94,92 @@ function viewRoles() {
     });
 }
 
+function newEmployee() {
+    inquirer.prompt([{
+            type: "input",
+            message: "What is the employees first name?",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message: "What is the employees last name?",
+            name: "lastName"
+        },
+        {
+            type: "number",
+            message: "What is the employees role ID",
+            name: "roleId"
+        },
+        {
+            type: "number",
+            message: "What is the employees manager's ID?",
+            name: "roleId"
+        }
+    ]).then(function(res) {
+        connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+            if (err) throw err;
+            console.table("Successfully Inserted", data);
+            askQuestions();
+        })
+    })
+}
+
+function newDepartment() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the name of the new department you want to add?",
+        name: "department"
+    }, ]).then(function(res) {
+        connection.query('INSERT INTO department (name) VALUES (?)', [res.department], function(err, data) {
+            if (err) throw err;
+            console.table("Successfully Inserted", data);
+            askQuestions();
+        })
+    })
+}
+
+
+function newRole() {
+    inquirer.prompt([
+        {
+            message: "enter title:",
+            type: "input",
+            name: "title"
+        }, {
+            message: "enter salary:",
+            type: "number",
+            name: "salary"
+        }, {
+            message: "enter department ID:",
+            type: "number",
+            name: "department_id"
+        }
+    ]).then(function (response) {
+        connection.query("INSERT INTO roles (title, salary, department_id) values (?, ?, ?)", [response.title, response.salary, response.department_id], function (err, data) {
+            console.table("Successfully Inserted",data);
+        })
+        askQuestions();
+    })
+
+}
+
+
+function updateRole() {
+    inquirer.prompt([
+        {
+            message: "which employee would you like to update? (use first name only for now)",
+            type: "input",
+            name: "name"
+        }, {
+            message: "enter the new role ID:",
+            type: "number",
+            name: "role_id"
+        }
+    ]).then(function (response) {
+        connection.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [response.role_id, response.name], function (err, data) {
+            console.table("Successfully Inserted", data);
+        })
+        askQuestions();
+    })
+
+}
