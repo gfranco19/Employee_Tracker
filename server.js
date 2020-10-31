487871544181d7ffc1f5d061c7243cfaf377040f
Giovanny Franco <gfranco19@live.com>
2020-10-30 23:06:02 -0700

    first inquirer questions

--- server.js
+++ server.js
@@ -14,3 +14,51 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId + "\n");
     questions();
 });
+
+function questions() {
+    inquirer.prompt({
+        message: "How can i help you today?",
+        type: "list",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "Add employee",
+            "Add department",
+            "Add role",
+            "Update employee role",
+            "RETURN"
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "View all employees":
+                viewEmployees()
+                break;
+
+            case "View all departments":
+                veiwDepartments()
+                break;
+
+            case "Add employee":
+                newEmployee()
+                break;
+
+            case "Add department":
+                newDepartment()
+                break;
+
+            case "Add role":
+                newRole()
+                break;
+
+            case "update employee role":
+                updateRole();
+                break;
+
+            default:
+                connection.end()
+                break;
+        }
+    })
+}
