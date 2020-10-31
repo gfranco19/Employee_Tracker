4106e5b928ea5805a1932ad9a518c7db7e9c0f92
Giovanny Franco <gfranco19@live.com>
2020-10-30 22:57:14 -0700

    sql files and connection on js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Clintb00.",
+    database: "employee_trackerdb"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    questions();
+});
